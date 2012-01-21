64b4bbd61149fba063461c54e9ced17015638aa8
Steve Losh <steve@stevelosh.com>
2012-01-21 12:14:16 -0500

    Initial commit.

---
+++ src/typkov/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns typkov.views.welcome
+  (:require [typkov.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to typkov"]))
