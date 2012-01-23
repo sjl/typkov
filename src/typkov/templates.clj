e2f0334671809f57cf13a8ebda58cf47e2e123e4
Steve Losh <steve@stevelosh.com>
2012-01-22 19:05:41 -0500

    More.
    Sorry about the shitty commit messages.

--- src/typkov/templates.clj
+++ src/typkov/templates.clj
@@ -1,9 +1,11 @@
 (ns typkov.templates
   (:require [noir.validation :as valid])
   (:use [noir.core :only [defpartial]]
+        [hiccup.form-helpers :only [hidden-field]]
         [hiccup.page-helpers :only [include-css html5 include-js link-to]]))
 
 
+; Utils -----------------------------------------------------------------------
 (def gauges "
   <script type='text/javascript'>
     var _gauges = _gauges || [];
@@ -19,7 +21,19 @@
     })();
   </script>")
 
+(defpartial select [id name options selected]
+  [:select {:name name :id id}
+   (map (fn [o]
+          [:option (merge {:value o}
+                          (when (= selected o) {:selected "selected"}))
+           o])
+        options)])
 
+(defpartial error-item [[first-error]]
+  [:div.alert-message.error first-error])
+
+
+; Base ------------------------------------------------------------------------
 (defpartial base [title & content]
   (html5
     [:head
@@ -30,11 +44,9 @@
     [:body
      [:div#content
       [:header
-       [:h1 "Typkov"]
+       [:h1 (link-to "/" "Typkov")]
        [:h2
-        "Create "
-        (link-to "http://www.gnu.org/software/gtypist/" "gtypist")
-        " lessons from your own writing!"]]
+        "Create typing lessons from your own writing!"]]
       content
       [:footer
        [:p
@@ -50,42 +62,72 @@
       gauges]]))
 
 
-(defpartial error-item [[first-error]]
-  [:div.alert-message.error first-error])
+; Home ------------------------------------------------------------------------
+(defpartial main-copy []
+  (list
+    [:p
+     "Typing practice is great, but wouldn't it be wonderful to "
+     "practice with the kind of words you usually use in your own writing?"]
+    [:p
+     "Enter some text and we'll use it to create a "
+     (link-to "http://www.gnu.org/software/gtypist/" "gtypist")
+     " or "
+     (link-to "http://ktouch.sourceforge.net/" "ktouch")
+     " lesson file for you."]))
+
+(defpartial home-form [text num format]
+  (list
+    (main-copy)
+    [:form {:method "post" :action "" :class "form-stacked"}
+     [:fieldset.text
+      [:label {:for "text"}
+       "Paste in some text you've written:"]
+      (valid/on-error :text error-item)
+      [:textarea#text {:name "text"} text]]
+     [:fieldset.format
+      [:label {:for "format"}
+       "Which typing program do you use?"]
+      (valid/on-error :format error-item)
+      (select "format" "format" ["gtypist" "ktouch"] format)]
+     [:fieldset.num
+      [:label {:for "num"}
+       "How many drills do you want?"]
+      (valid/on-error :num error-item)
+      (select "num" "num" (range 10 51 10) num)]
+     [:button {:type "submit" :class "btn primary"} "Get a Lesson"]]))
 
 
-(defpartial home [text num lesson]
+(defpartial usage-gtypist []
+  [:p
+   "Copy all of the following into a "
+   [:code "whatever.typ"]
+   " file and run "
+   [:code "gtypist whatever.typ"]
+   " to use it!"])
+
+(defpartial usage-ktouch []
+  [:p
+   "Copy all of the following into a "
+   [:code "whatever.ktouch"]
+   " file and load it up in ktouch to use it!"])
+
+(defpartial home-lesson [text num format lesson]
+  (list
+    (case format
+      "gtypist" (usage-gtypist)
+      "ktouch" (usage-ktouch))
+    [:form.noop
+     [:textarea.lesson lesson]]
+    [:form {:method "post" :action ""}
+     [:button.btn {:type "submit"} "Start Over"]
+     (hidden-field "restart" "1")
+     (hidden-field "text" text)
+     (hidden-field "num" num)
+     (hidden-field "format" format)]))
+
+
+(defpartial home [text num format lesson]
   (base "Typkov"
-        [:p
-         "Typing practice is great, but wouldn't it be wonderful to "
-         "practice with the kind of words you usually use in your own writing?"]
-        [:p
-         "Enter some text and we'll create a "
-         (link-to "http://www.gnu.org/software/gtypist/" "gtypist")
-         " lesson based on it for you."]
-        [:form {:method "post" :action "" :class "form-stacked"}
-         [:fieldset.text
-          [:label {:for "text"}
-           "Paste in some text you've written:"]
-          (valid/on-error :text error-item)
-          [:textarea#text {:name "text"} text]]
-         [:fieldset.num
-          [:label {:for "num"}
-           "How many drills do you want?"]
-          (valid/on-error :num error-item)
-          [:select#num {:name "num"}
-           (map (fn [n]
-                  [:option (merge {:value n}
-                                  (when (= num n) {:selected "selected"}))
-                   n])
-                (range 10 51 10))]]
-         [:button {:type "submit" :class "btn primary"} "Get a Lesson"]]
-        (when lesson
-          (list
-            [:p
-             "Copy all of the following into a "
-             [:code "whatever.typ"]
-             " file and run "
-             [:code "gtypist whatever.typ"]
-             " to use it!"]
-            [:textarea.lesson lesson]))))
+        (if lesson
+          (home-lesson text num format lesson)
+          (home-form text num format))))
